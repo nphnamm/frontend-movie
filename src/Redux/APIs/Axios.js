b76cfb78d09e35aae31e1a253dd0f1cf9a31bd38
nphnamm <80657434+nphnamm@users.noreply.github.com>
2024-06-14 18:56:53 +0700

    add new code

--- src/Redux/APIs/Axios.js
+++ src/Redux/APIs/Axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: "https://backend-movie-9oro.onrender.com",
+  baseURL: "https://backend-movie-9oro.onrender.com/api",
 });
 export default Axios;
